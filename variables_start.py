baa6bd0354d560be074de75e3bf755c60144e1f1
Aaron Cleveland <toscleveland@gmail.com>
2021-03-28 01:28:13 -0500

    Variables Done

--- variables_start.py
+++ variables_start.py
@@ -3,17 +3,32 @@
 #
 
 # Declare a variable and initialize it
+f = 0
+print(f)
 
 
 
 # re-declaring the variable works
-
+f = "abc"
+print(f)
 
 
 # ERROR: variables of different types cannot be combined
-
+# print("this is a string " + 123) # will not work
+print("this is a string " + str(123)) # will work
 
 
 # Global vs. local variables in functions
+def someFunction():
+    # local variable
+    # f = "def"
+    # global variable change
+    global f
+    f = "def"
+    print(f)
 
+someFunction()
+print(f)
 
+del f
+print(f)
